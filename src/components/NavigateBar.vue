a15e7357c87951aeb9a669f34800f67add863756
ForzenString <964413011@qq.com>
2023-07-16 19:37:15 +0800

    格式化

--- src/components/NavigateBar.vue
+++ src/components/NavigateBar.vue
@@ -1,40 +1,49 @@
 <script lang="ts" setup>
-import {ref} from "vue";
-import {useRouter} from "vue-router";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 const property = defineProps<{
-  title?: string,
-  selectItems: { text: string, name: string }[]
-}>()
+  title?: string;
+  selectItems: { text: string; name: string }[];
+}>();
 
-const selectItems = property.selectItems.map((value,idx,_)=>{return {idx,key:value.name,value:value.text}})
-const drawer = ref(false)
-const router = useRouter()
-const selectItem = ({id, _value, _path}) => {
-  console.log(id)
-  router.push({name:id})
-  expandDrawer()
-}
+const selectItems = property.selectItems.map((value, idx, _) => {
+  return { idx, key: value.name, value: value.text };
+});
+const drawer = ref(false);
+const router = useRouter();
+const selectItem = (id: any) => {
+  console.log(id);
+  router.push({ name: id });
+  expandDrawer();
+};
 
 const expandDrawer = () => {
-  console.log("expand" + drawer)
-  drawer.value = !drawer.value
-}
+  console.log("expand" + drawer);
+  drawer.value = !drawer.value;
+};
 </script>
 
 <template>
   <v-app-bar :elevation="3" color="primary" density="compact">
     <template v-slot:prepend>
-      <v-app-bar-nav-icon variant="text" @click="expandDrawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        variant="text"
+        @click="expandDrawer"
+      ></v-app-bar-nav-icon>
     </template>
     <v-app-bar-title>语音识别</v-app-bar-title>
   </v-app-bar>
 
   <v-navigation-drawer v-model="drawer" :temporary="true" location="left">
-    <v-list :items="selectItems" item-title="value" item-value="key" @click:select="selectItem"></v-list>
+    <v-list
+      :items="selectItems"
+      item-title="value"
+      item-value="key"
+      @click:select="selectItem"
+    ></v-list>
   </v-navigation-drawer>
 </template>
 
 <style scoped>
-
 </style>
