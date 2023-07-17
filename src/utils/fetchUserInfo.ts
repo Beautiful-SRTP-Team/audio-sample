1997d0ae3457ee51b79bf68f0b3363b41326be78
FrozenString <964413011@qq.com>
2023-07-17 15:21:34 +0800

    use url

--- src/utils/fetchUserInfo.ts
+++ src/utils/fetchUserInfo.ts
@@ -1,8 +1,14 @@
 export async function fetchUserInfo(options: {
   userId: string;
 }): Promise<{ level: number; name: string; exp: number }> {
+
+  let url = new URL("http://127.0.0.1:4523/m1/2536058-0-default/get-user-info")
+  url.searchParams.append("userId",options.userId)
+
+  console.log(url);
+  
   const resp = await fetch(
-    `http://127.0.0.1:4523/m1/2536058-0-default/get-user-info?userId=${options.userId}`,
+    url,
     {
       method: "GET",
     }
